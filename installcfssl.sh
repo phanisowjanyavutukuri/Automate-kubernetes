ccf4ed947051fc62c4b88869e3f13dc6383873b9
Sowjanya Vutukuri <sowjanya.vutukuri@ggktech.local>
2018-12-28 15:57:46 +0530

    added script

---
+++ installcfssl.sh
@@ -0,0 +1,23 @@
+file="/usr/local/bin/cfssljson"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	wget -q --show-progress --https-only --timestamping \
+  https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 \
+  https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+
+  chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
+  sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
+  sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+
+fi
+file="/usr/local/bin/kubectl"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	wget https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kubectl
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/
+fi
